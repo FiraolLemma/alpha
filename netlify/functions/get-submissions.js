284aa3cc6f2330479ff01118093649a6ab44a61c
fraol lemma <fraollemma0506@gmail.com>
2025-08-01 15:44:58 +0300

    day 5

--- netlify/functions/get-submissions.js
+++ netlify/functions/get-submissions.js
@@ -1,28 +1,15 @@
-const fetch = require('node-fetch');
-
 exports.handler = async (event) => {
-  // Password protection
   const { password } = JSON.parse(event.body);
-  if (password !== "YOUR_SECRET_PASSWORD") {
+  
+  // Verify password (store this in environment variables!)
+  if (password !== process.env.ADMIN_PW) {
     return { statusCode: 401, body: "Unauthorized" };
   }
-
-  try {
-    const response = await fetch(
-      `https://api.netlify.com/api/v1/sites/${process.env.SITE_ID}/submissions`,
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.API_AUTH_TOKEN}`
-        }
-      }
-    );
-    
-    const data = await response.json();
-    return {
-      statusCode: 200,
-      body: JSON.stringify(data)
-    };
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() };
-  }
+  
+  const response = await fetch(
+    `https://api.netlify.com/api/v1/sites/${process.env.SITE_ID}/submissions`,
+    { headers: { Authorization: `Bearer ${process.env.API_AUTH_TOKEN}` } }
+  );
+  
+  return { statusCode: 200, body: JSON.stringify(await response.json()) };
 };
